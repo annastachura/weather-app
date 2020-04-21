db90b22ee8b250ed1e4bfc3840ae16fbf749c5db
Anna Stachura <annas19914@gmail.com>
2020-04-21 13:13:38 +0200

    hours api

--- src/js/main.js
+++ src/js/main.js
@@ -15,20 +15,62 @@ if ('serviceWorker' in navigator) {
 }
 
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Krakow&APPID=b77d6549110ac17233e88b98716d9e61&units=metric')
+// fetch('http://api.openweathermap.org/data/2.5/weather?q=Krakow&APPID=b77d6549110ac17233e88b98716d9e61&units=metric')
+//     .then(resp => resp.json())
+//     .then(resp => {
+//         resp.main.temp
+//         temperature.innerHTML = Math.round(resp.main.temp);
+//         humidity.innerHTML = resp.main.humidity;
+//         pressure.innerHTML = resp.main.pressure;
+//         wind.innerHTML = resp.wind.speed;
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+
+// const temperature = document.querySelector('.now__temperature--js');
+// const humidity = document.querySelector('.now__condition--humidity');
+// const pressure = document.querySelector(".now__condition--pressure");
+// const wind = document.querySelector(".now__condition--wind");
+
+
+fetch('http://api.openweathermap.org/data/2.5/forecast/?q=London,uk&APPID=b77d6549110ac17233e88b98716d9e61&units=metric')
     .then(resp => resp.json())
     .then(resp => {
-        resp.main.temp
-        temperature.innerHTML = Math.round(resp.main.temp);
-        humidity.innerHTML = resp.main.humidity;
-        pressure.innerHTML = resp.main.pressure;
-        wind.innerHTML = resp.wind.speed;
+        const forecastList = resp.list;
+
+
+        for (const forecast of forecastList) {
+            console.log(forecast);
+            hour.innerHTML += `<li class="hour__list--item">
+            <span class="hour__list--time">${hours}</span>
+            <span class="hour__list--temp">${Math.round(forecast.main.temp)}°C</span>
+        </li>`
+
+            var date = new Date(forecast.dt * 1000);
+            var hours = date.getHours();
+            console.log(hours);
+
+        }
     })
     .catch(err => {
         console.log(err);
     })
 
-const temperature = document.querySelector('.now__temperature--js');
-const humidity = document.querySelector('.now__condition--humidity');
-const pressure = document.querySelector(".now__condition--pressure");
-const wind = document.querySelector(".now__condition--wind");
+const timeVisible = document.querySelector(".today");
+const hourSection = document.querySelector(".hour");
+const hour = document.querySelector(".hour__list");
+const time = document.querySelector(".hour__list--time");
+hour.innerHTML = "";
+
+const everyHour = document.querySelector(".list__element--hour");
+everyHour.addEventListener('click', function() {
+    hourSection.classList.add("hour--visible");
+    timeVisible.classList.remove("today--visible");
+});
+
+const now = document.querySelector(".list__element--now");
+now.addEventListener('click', function() {
+    timeVisible.classList.add("today--visible");
+    hourSection.classList.remove("hour--visible");
+})
